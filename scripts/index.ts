5e817ecca5c6a5ec9468a38c1e6de93e7b1bd641
yuliyu123 <looperx95@gmail.com>
2023-02-16 16:02:58 +0800

    update

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,15 +1,9 @@
-import { deploy, start } from "./arb"
-
 
 async function main() {
-    await deploy();
-    await start();
+
 }
 
-main()
-    .then((_) => {
-        console.log("Finished!!!");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
