c9a01798c3ec1291a4dd48997d2e93f0ba5c05f6
yuliyu123 <looperx95@gmail.com>
2023-05-29 20:36:51 +0800

    1

--- src/interfaces/IArb.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-interface IArb {
-    function startArb() external returns (uint256);
-    function dexTrade(bytes memory _data) external;
-    function receiveLoan(bytes memory _data) external;
-}
--- src/interfaces/IWETH.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-interface IWETH {
-    function deposit() external payable;
-
-    function withdraw(uint256 wad) external;
-}
